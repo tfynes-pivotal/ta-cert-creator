ae07adb718b155ead727e4d9c15fd11d0fd2abbb
Mark Fynes <tfynes@vmware.com>
2023-09-29 15:16:41 -0400

    .

---
+++ generate-cert.sh
@@ -0,0 +1,15 @@
+
+# certbot certonly \
+# --server https://acme-v02.api.letsencrypt.org/directory
+# --agree-tos \
+# --cert-name homelab.fynesy.com \
+# -d '*.uaa.system.homelab.fynesy.com'
+# -m tmfynes@yahoo.com 
+# --config-dir ./config 
+# --logs-dir ./logs 
+# --work-dir ./work 
+# --manual 
+# --preferred-challenges dns
+
+
+CERTBOT_COMMAND
